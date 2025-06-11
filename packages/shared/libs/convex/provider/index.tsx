13c23396e47f7b88f36c898474fa6520cd3a7d49
codynog <yoneyoneyone99@gmail.com>
2025-06-12 01:50:52 +0900

    fix convex client

--- packages/shared/libs/convex/provider/index.tsx
+++ packages/shared/libs/convex/provider/index.tsx
@@ -6,16 +6,16 @@ type ConvexAppProviderProps = {
   children: ReactNode;
 };
 
+const client = new ConvexReactClient(CONVEX_URL, {
+  unsavedChangesWarning: false,
+});
+
 export const ConvexAppProvider = ({ children }: ConvexAppProviderProps) => {
   if (!CONVEX_URL) {
     throw new Error(
       "Convex URL is not set. Please set CONVEX_URL environment variable",
     );
   }
-
-  const client = new ConvexReactClient(CONVEX_URL, {
-    unsavedChangesWarning: false,
-  });
 
   return <ConvexProvider client={client}>{children}</ConvexProvider>;
 };
